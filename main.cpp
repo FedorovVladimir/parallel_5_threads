d90d4b58d526f8c55ae82e42f213ea1b94d33e0b
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-05-20 22:10:43 +0700

    add thread

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+DWORD WINAPI ThreadProc(PVOID arg) {
+    cout << "hello 2\n";
+    ExitThread(0);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Start program!\n";
+
+    HANDLE thread1 = CreateThread(nullptr, 0, ThreadProc, (PVOID) 1, 0, nullptr);
+
+    cout << "End program!\n";
+    WaitForSingleObject(thread1, INFINITE);
     return 0;
 }
