06cad592ae5f26b3c3c4a9bff6b2b44be9bc484c
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-05-20 22:15:57 +0700

    add farm thread

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 
 DWORD WINAPI MarketThreadProc(PVOID arg) {
-    cout << "Market start!";
+    cout << "Market start!\n";
+    ExitThread(0);
+}
+
+DWORD WINAPI FarmThreadProc(PVOID arg) {
+    cout << "Farm start!\n";
     ExitThread(0);
 }
 
@@ -13,8 +18,10 @@ int main() {
     cout << "Start program!\n";
 
     HANDLE marketThread = CreateThread(nullptr, 0, MarketThreadProc, (PVOID) 1, 0, nullptr);
+    HANDLE farmThread = CreateThread(nullptr, 0, FarmThreadProc, (PVOID) 1, 0, nullptr);
 
     cout << "End program!\n";
     WaitForSingleObject(marketThread, INFINITE);
+    WaitForSingleObject(farmThread, INFINITE);
     return 0;
 }
